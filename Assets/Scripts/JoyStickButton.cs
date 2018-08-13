f058a4a29704b6a5b852bd0742f30a7e7a865255
homer <homer_llasag1@yahoo.com>
2018-08-13 16:45:06 +0100

    first commit

---
+++ Assets/Scripts/JoyStickButton.cs
@@ -0,0 +1,59 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStickButton : MonoBehaviour, IInputHandler, IInputClickHandler, IManipulationHandler
+{
+    public float x;
+    public float y;
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        SocketConnection.getInstance().emitData(x, y);
+    }
+    public void OnInputDown(InputEventData eventData)
+    {
+       // Debug.Log("OnInputDown");
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+       // Debug.Log("OnInputUp");
+    }
+
+    public void OnManipulationCanceled(ManipulationEventData eventData)
+    {
+       // Debug.Log("OnManipulationCanceled");
+    }
+
+    public void OnManipulationCompleted(ManipulationEventData eventData)
+    {
+       // Debug.Log("OnManipulationCompleted");
+    }
+
+    public void OnManipulationStarted(ManipulationEventData eventData)
+    {
+      //  Debug.Log("OnManipulationStarted");
+    }
+
+    public void OnManipulationUpdated(ManipulationEventData eventData)
+    {
+        SocketConnection.getInstance().emitData(x,y);
+    }
+
+
+
+    
+
+
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/models/SocketConnection.cs
@@ -0,0 +1,46 @@
+﻿
+//using Quobject.SocketIoClientDotNet.Client;
+using UnityEngine;
+public class SocketConnection {
+    public static SocketConnection instance = null;
+    //private Socket socket = null;
+    private string serverURL = "http://192.168.140.110:8080";
+    private SocketConnection() {
+        //this.socket = IO.Socket(serverURL);
+        //this.socket.On(Socket.EVENT_CONNECT, () => {
+        //          // Access to Unity UI is not allowed in a background thread, so let's put into a shared variable
+        //        //  Debug.Log("Socket.IO connected.");
+
+        //  });
+    }
+    public static SocketConnection getInstance() {
+        if (instance == null)
+        {
+            instance = new SocketConnection();
+
+        }
+        return instance;
+    }
+    public void emitData(float x, float y)
+    {
+        Debug.Log("Emiting x:"+x+ " y:"+y);
+    }
+    //public Socket getSocket() {
+    //    return this.socket;
+    //}
+    //public void setDisconnectSocket() {
+    //    if (instance != null)
+    //    {
+    //        this.socket.Disconnect();
+            
+    //    }
+    //}
+    //public void setNullSocket()
+    //{
+    //    if (instance != null)
+    //    {
+    //        this.socket = null;
+
+    //    }
+    //}
+}
