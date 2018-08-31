e35ff4fb612389d562922ce640364c22141c7859
homer <homer_llasag1@yahoo.com>
2018-08-31 21:03:09 +0100

    adding socket TCP listener

---
+++ Assets/Scripts/services/SocketClient.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+#if !UNITY_EDITOR
+using System.Threading.Tasks;
+#endif
+
+public class SocketClient: MonoBehaviour
+{
+#if !UNITY_EDITOR
+    private bool _useUWP = true;
+    private Windows.Networking.Sockets.StreamSocket socket;
+    private Task exchangeTask;
+#endif
+
+#if UNITY_EDITOR
+    private bool _useUWP = false;
+    System.Net.Sockets.TcpClient client;
+    System.Net.Sockets.NetworkStream stream;
+    private Thread exchangeThread;
+#endif
+
+    private Byte[] bytes = new Byte[256];
+    private StreamWriter writer;
+    private StreamReader reader;
+    void Start()
+    {
+        Connect("192.168.1.3", "8080");
+    }
+    public void Connect(string host, string port)
+    {
+        if (_useUWP)
+        {
+            Debug.Log("useUWP:"+_useUWP);
+            ConnectUWP(host, port);
+        }
+        else
+        {
+            Debug.Log("useUWP:" + _useUWP);
+            ConnectUnity(host, port);
+        }
+    }
+
+
+
+#if UNITY_EDITOR
+    private void ConnectUWP(string host, string port)
+#else
+    private async void ConnectUWP(string host, string port)
+#endif
+    {
+#if UNITY_EDITOR
+        Debug.Log("UWP TCP client used in Unity!");
+#else
+        try
+        {
+            if (exchangeTask != null) StopExchange();
+        
+            socket = new Windows.Networking.Sockets.StreamSocket();
+            Windows.Networking.HostName serverHost = new Windows.Networking.HostName(host);
+            await socket.ConnectAsync(serverHost, port);
+        
+            Stream streamOut = socket.OutputStream.AsStreamForWrite();
+            writer = new StreamWriter(streamOut) { AutoFlush = true };
+        
+            Stream streamIn = socket.InputStream.AsStreamForRead();
+            reader = new StreamReader(streamIn);
+
+            RestartExchange();
+            successStatus = "Connected!";
+        }
+        catch (Exception e)
+        {
+            errorStatus = e.ToString();
+        }
+#endif
+    }
+
+    private void ConnectUnity(string host, string port)
+    {
+#if !UNITY_EDITOR
+        errorStatus = "Unity TCP client used in UWP!";
+#else
+        try
+        {
+            if (exchangeThread != null) StopExchange();
+
+            client = new System.Net.Sockets.TcpClient(host, Int32.Parse(port));
+            stream = client.GetStream();
+            reader = new StreamReader(stream);
+            writer = new StreamWriter(stream) { AutoFlush = true };
+
+            RestartExchange();
+            
+            Debug.Log("Connected!");
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Error:"+ e.ToString());
+           
+        }
+#endif
+    }
+
+    private bool exchanging = false;
+    private bool exchangeStopRequested = false;
+    private string lastPacket = null;
+    public void RestartExchange()
+    {
+#if UNITY_EDITOR
+        if (exchangeThread != null) StopExchange();
+        exchangeStopRequested = false;
+        exchangeThread = new System.Threading.Thread(ExchangePackets);
+        exchangeThread.Start();
+#else
+        if (exchangeTask != null) StopExchange();
+        exchangeStopRequested = false;
+        exchangeTask = Task.Run(() => ExchangePackets());
+#endif
+    }
+
+    public void Update()
+    {
+        if (lastPacket != null)
+        {
+            ReportDataToTrackingManager(lastPacket);
+        }
+    }
+
+    public void ExchangePackets()
+    {
+        while (!exchangeStopRequested)
+        {
+            if (writer == null || reader == null) continue;
+            exchanging = true;
+
+            writer.Write("X\n");
+            Debug.Log("Sent data!");
+            string received = null;
+
+#if UNITY_EDITOR
+            byte[] bytes = new byte[client.SendBufferSize];
+            int recv = 0;
+            while (true)
+            {
+                recv = stream.Read(bytes, 0, client.SendBufferSize);
+                received += Encoding.UTF8.GetString(bytes, 0, recv);
+                if (received.EndsWith("\n")) break;
+            }
+#else
+            received = reader.ReadLine();
+#endif
+
+            lastPacket = received;
+            Debug.Log("Read data: " + received);
+
+            exchanging = false;
+        }
+    }
+
+    private void ReportDataToTrackingManager(string data)
+    {
+        if (data == null)
+        {
+            Debug.Log("Received a frame but data was null");
+            return;
+        }
+    }
+
+    
+
+    public void StopExchange()
+    {
+        exchangeStopRequested = true;
+
+#if UNITY_EDITOR
+        if (exchangeThread != null)
+        {
+            exchangeThread.Abort();
+            stream.Close();
+            client.Close();
+            writer.Close();
+            reader.Close();
+
+            stream = null;
+            exchangeThread = null;
+        }
+#else
+        if (exchangeTask != null) {
+            exchangeTask.Wait();
+            socket.Dispose();
+            writer.Dispose();
+            reader.Dispose();
+
+            socket = null;
+            exchangeTask = null;
+        }
+#endif
+        writer = null;
+        reader = null;
+    }
+
+    public void OnDestroy()
+    {
+        StopExchange();
+    }
+
+}
---
+++ Assets/Scripts/services/USStatusTextManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+public class USStatusTextManager
+{
+    internal void SetError(string errorStatus)
+    {
+        Debug.Print(errorStatus);
+    }
+
+    internal void SetWarning(string warningStatus)
+    {
+        Debug.Print(warningStatus);
+    }
+
+    internal void SetSuccess(string successStatus)
+    {
+        Debug.Print(successStatus);
+    }
+
+    internal void SetUnknown(string unknownStatus)
+    {
+        Debug.Print(unknownStatus);
+    }
+}
